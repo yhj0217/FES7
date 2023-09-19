ffd6237b5d14e6d6929ea2328f1ec442e748becb
HeeJoon.Yang <yhj2151@gmail.com>
2023-09-19 17:53:52 +0900

    React

--- 20230918/10000/src/App.js
+++ 20230918/10000/src/App.js
@@ -2,14 +2,17 @@ import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
 import Modal from "./components/modal/Modal";
+import { useState } from "react";
 
 function App() {
+  const [modalShow, setModalShow] = useState(false);
+
   return (
     <div id="app">
       <Header />
-      <Main />
+      <Main setModalShow={setModalShow} />
       <Footer />
-      <Modal />
+      {modalShow && <Modal setModalShow={setModalShow} />}
     </div>
   );
 }
