e089857fbd88cf883a6d4379af01bbad26c3f12f
HeeJoon.Yang <yhj2151@gmail.com>
2023-09-20 23:51:59 +0900

    React

---
+++ 20230920/hook-usememo/src/App4.js
@@ -0,0 +1,10 @@
+import { useContext, createContext } from "react";
+import HelloLicat from "./components/HelloLicat";
+
+// const UserInfo = createContext({ name: "gary", id: "garyIsFree" });
+
+const App = () => {
+  return <HelloLicat />;
+};
+
+export default App;
