4274f3a8e397ef1291543fbe0f4288be63064a34
HeeJoon.Yang <yhj2151@gmail.com>
2023-10-06 18:22:41 +0900

    감귤마켓 로그인/회원가입 구현실습

---
+++ 20231006/app/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import JoinPage from "./Components/JoinPage";
+import LoginPage from "./Components/LoginPage";
+
+function App() {
+  const [page, setPage] = useState(true);
+  const [info, setInfo] = useState("");
+  const handlePage = () => {
+    setPage((prevPage) => {
+      return !prevPage;
+    });
+  };
+  const getMyInfo = async () => {
+    const token = localStorage.getItem("token");
+    console.log(token);
+    const res = await fetch("https://api.mandarin.weniv.co.kr/user/myinfo", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const json = await res.json();
+    console.log(json);
+    setInfo(JSON.stringify(json));
+  };
+  return (
+    <div>
+      <button type="button" onClick={getMyInfo}>
+        내 정보 불러오기
+      </button>
+      {info}
+      {page ? (
+        <LoginPage handlePage={handlePage} />
+      ) : (
+        <JoinPage handlePage={handlePage} />
+      )}
+    </div>
+  );
+}
+export default App;
